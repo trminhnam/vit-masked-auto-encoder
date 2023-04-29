710452a400639eaefdc1c23ee5d715535d702680
trminhnam <trminhnam20082002@gmail.com>
2023-04-29 23:38:57 +0700

    feat: customize mask autoencoder vit

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from utils import get_2d_sincos_pos_embed
 
 
 class PatchEmbedding(nn.Module):
@@ -37,6 +38,9 @@ class PatchEmbedding(nn.Module):
         x = x.transpose(1, 2)  # (batch_size, n_patches, embedding_dim)
 
         return x
+
+    def get_num_patches(self):
+        return self.n_patches
 
 
 class Attention(nn.Module):
@@ -250,61 +254,297 @@ class ViT(nn.Module):
         return x
 
 
-# # implement ViT
-# class ViT(nn.Module):
-#     def __init__(
-#         self,
-#         image_size=224,
-#         patch_size=16,
-#         num_classes=1000,
-#         dim=768,
-#         depth=12,
-#         heads=12,
-#         mlp_dim=3072,
-#         dropout=0.1,
-#         emb_dropout=0.1,
-#     ):
-#         super(ViT, self).__init__()
-#         num_patches = (image_size // patch_size) ** 2
-#         patch_dim = 3 * patch_size**2
+class MaskedAutoencoderViT(nn.Module):
+    """Masked Autoencoder with VisionTransformer backbone"""
 
-#         self.patch_size = patch_size
-#         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-#         self.patch_to_embedding = nn.Linear(patch_dim, dim)
-#         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-#         self.transformer = Transformer(dim, depth, heads, mlp_dim, dropout)
-#         self.to_cls_token = nn.Identity()
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        in_channels=3,
+        num_classes=1000,
+        embedding_dim=1024,
+        depth=24,
+        n_heads=16,
+        decoder_embed_dim=512,
+        decoder_depth=8,
+        decoder_num_heads=16,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        norm_layer=nn.LayerNorm,
+        norm_pix_loss=False,
+        attn_p=0.0,
+        p=0.0,
+    ):
+        super().__init__()
 
-#         self.mlp_head = nn.Sequential(nn.LayerNorm(dim), nn.Linear(dim, num_classes))
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.patch_embed = PatchEmbedding(
+            img_size, patch_size, in_channels, embedding_dim
+        )
+        num_patches = self.patch_embed.get_num_patches()
 
-#     def forward(self, x):
-#         b, c, h, w = x.shape
-#         assert h == w, "image height and width must be equal"
-#         assert h % self.patch_size == 0, "image height must be multiple of patch size"
-#         p = self.patch_size
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embedding_dim))
+        self.pos_embed = nn.Parameter(
+            torch.zeros(1, num_patches + 1, embedding_dim), requires_grad=False
+        )  # fixed sin-cos embedding
 
-#         # flatten the image
-#         x = x.reshape(b, c, h // p, p, w // p, p)
-#         x = x.permute(0, 2, 4, 1, 3, 5)
-#         x = x.reshape(b, -1, c * p * p)
+        self.blocks = nn.ModuleList(
+            [
+                Block(
+                    embedding_dim,
+                    n_heads,
+                    mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    attn_p=attn_p,
+                    proj_p=p,
+                )
+                for _ in range(depth)
+            ]
+        )
+        self.norm = norm_layer(embedding_dim)
+        self.cls_head = nn.Linear(embedding_dim, num_classes)  # classification head
+        # --------------------------------------------------------------------------
 
-#         # patch to embedding
-#         x = self.patch_to_embedding(x)
+        # --------------------------------------------------------------------------
+        # MAE decoder specifics
+        self.decoder_embed = nn.Linear(embedding_dim, decoder_embed_dim, bias=True)
 
-#         # concatenate cls token
-#         cls_tokens = self.cls_token.expand(b, -1, -1)
-#         x = torch.cat((cls_tokens, x), dim=1)
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
 
-#         # add position embedding
-#         x += self.pos_embedding[:, : (x.shape[1])]
+        self.decoder_pos_embed = nn.Parameter(
+            torch.zeros(1, num_patches + 1, decoder_embed_dim), requires_grad=False
+        )  # fixed sin-cos embedding
 
-#         # transformer
-#         x = self.transformer(x)
+        self.decoder_blocks = nn.ModuleList(
+            [
+                Block(
+                    decoder_embed_dim,
+                    decoder_num_heads,
+                    mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    attn_p=attn_p,
+                    proj_p=p,
+                )
+                for _ in range(decoder_depth)
+            ]
+        )
 
-#         # take cls token
-#         x = self.to_cls_token(x[:, 0])
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(
+            decoder_embed_dim, patch_size**2 * in_channels, bias=True
+        )  # decoder to patch
+        # --------------------------------------------------------------------------
 
-#         # mlp head
-#         x = self.mlp_head(x)
+        self.norm_pix_loss = norm_pix_loss
 
-#         return x
+        self.initialize_weights()
+
+    def forward(self, imgs, stage, mask_ratio=0.75):
+        if stage == "pretrain":
+            latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio, stage)
+            pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+            loss = self.forward_loss(imgs, pred, mask)
+            # return loss, pred, mask
+            return {"loss": loss, "pred": pred, "mask": mask}
+
+        elif stage == "finetune":
+            latent, _, _ = self.forward_encoder(imgs, mask_ratio, stage)
+            cls_token = latent[:, 0, :]
+            pred_cls = self.cls_head(cls_token)
+            # return pred_cls
+            return {"pred_cls": pred_cls}
+
+        elif stage == "combine":
+            latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio, stage)
+            pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+            loss = self.forward_loss(imgs, pred, mask)
+            cls_token = latent[:, 0, :]
+            pred_cls = self.cls_head(cls_token)
+            # return loss, pred, mask, pred_cls
+            return {
+                "loss": loss,
+                "pred": pred,
+                "mask": mask,
+                "pred_cls": pred_cls,
+            }
+
+        else:
+            raise ValueError("stage must be one of 'pretrain', 'finetune', 'combine'")
+
+    def classify(self, imgs):
+        latent, _, _ = self.forward_encoder(imgs, mask_ratio=0.75, stage="finetune")
+        cls_token = latent[:, 0, :]
+        pred = self.cls_head(cls_token)
+        return pred
+
+    def predict(self, imgs):
+        return self.classify(imgs)
+
+    def forward_encoder(self, x, mask_ratio, stage):
+        # embed patches
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        # masking: length -> length * mask_ratio
+        if stage != "finetune":
+            x, mask, ids_restore = self.random_masking(x, mask_ratio)
+        else:
+            mask = None
+            ids_restore = None
+
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x, mask, ids_restore
+
+    def forward_decoder(self, x, ids_restore):
+        # embed tokens
+        x = self.decoder_embed(x)
+
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(
+            x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1
+        )
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        x_ = torch.gather(
+            x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2])
+        )  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+
+        return x
+
+    def forward_loss(self, imgs, pred, mask):
+        """
+        imgs: [N, 3, H, W]
+        pred: [N, L, p*p*3]
+        mask: [N, L], 0 is keep, 1 is remove,
+        """
+        target = self.patchify(imgs)
+        if self.norm_pix_loss:
+            mean = target.mean(dim=-1, keepdim=True)
+            var = target.var(dim=-1, keepdim=True)
+            target = (target - mean) / (var + 1.0e-6) ** 0.5
+
+        loss = (pred - target) ** 2
+        loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
+
+        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        return loss
+
+    def initialize_weights(self):
+        num_patches = self.patch_embed.get_num_patches()
+        pos_embed = get_2d_sincos_pos_embed(
+            self.pos_embed.shape[-1],
+            int(num_patches**0.5),
+            cls_token=True,
+        )
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(
+            self.decoder_pos_embed.shape[-1],
+            int(num_patches**0.5),
+            cls_token=True,
+        )
+        self.decoder_pos_embed.data.copy_(
+            torch.from_numpy(decoder_pos_embed).float().unsqueeze(0)
+        )
+
+        w = self.patch_embed.projection_layer.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        torch.nn.init.normal_(self.cls_token, std=0.02)
+        torch.nn.init.normal_(self.mask_token, std=0.02)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def random_masking(self, x, mask_ratio):
+        """
+        Perform per-sample random masking by per-sample shuffling.
+        Per-sample shuffling is done by argsort random noise.
+        x: [N, L, D], sequence
+        """
+        N, L, D = x.shape  # batch, length, dim
+        len_keep = int(L * (1 - mask_ratio))
+
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+
+        # sort noise for each sample
+        ids_shuffle = torch.argsort(
+            noise, dim=1
+        )  # ascend: small is keep, large is remove
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        # keep the first subset
+        ids_keep = ids_shuffle[:, :len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+
+        return x_masked, mask, ids_restore
+
+    def patchify(self, imgs):
+        """
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
+        """
+        p = self.patch_embed.patch_size
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum("nchpwq->nhwpqc", x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 3))
+        return x
+
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.patch_embed.patch_size
+        h = w = int(x.shape[1] ** 0.5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum("nhwpqc->nchpwq", x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-from model import ViT
+import torch.nn as nn
+from model import ViT, MaskedAutoencoderViT
 
 
 def get_num_params(model):
@@ -17,10 +18,10 @@ def assert_tensors_equal(t1, t2):
 
 
 config = {
-    "img_size": 224,
-    "patch_size": 74,
+    "img_size": 48,
+    "patch_size": 16,
     "in_channels": 3,
-    "num_classes": 1000,
+    "num_classes": 2,
     "embedding_dim": 768,
     "depth": 12,
     "n_heads": 12,
@@ -30,11 +31,75 @@ config = {
     "p": 0.0,
 }
 
-model = ViT(**config)
-model.eval()
+# model = ViT(**config)
+# model.eval()
 
+# # inputs = torch.randn(1, 3, 224, 224)
+# # outputs = model(inputs)
 # inputs = torch.randn(1, 3, 224, 224)
-# outputs = model(inputs)
-inputs = torch.randn(1, 3, 224, 224)
-mask = torch.from_numpy(np.array([[1, 0, 1, 0, 1, 0, 0, 1, 0]]))
-outputs = model(inputs, q_mask=mask)
+# mask = torch.from_numpy(np.array([[1, 0, 1, 0, 1, 0, 0, 1, 0]]))
+# outputs = model(inputs, q_mask=mask)
+
+model = MaskedAutoencoderViT(**config)
+inputs = torch.randn(11, 3, 48, 48)
+targets = torch.randint(0, config["num_classes"], (11,))
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+inputs = inputs.to(device)
+targets = targets.to(device)
+
+# pretrained testing
+outputs = model(inputs, mask_ratio=0.5, stage="pretrain")
+print("Pretrained outputs test passed!")
+print("#" * 50 + "\n")
+
+# fine-tuning testing
+outputs = model(inputs, mask_ratio=0.5, stage="finetune")
+print("Fine-tuning outputs test passed!")
+print("#" * 50 + "\n")
+
+# combine testing
+outputs = model(inputs, mask_ratio=0.5, stage="combine")
+print("Combine outputs test passed!")
+print("#" * 50 + "\n")
+
+# test pretrained backprop
+print("Testing pretrained backprop...")
+for _ in range(10):
+    outputs = model(inputs, mask_ratio=0.5, stage="pretrain")
+    loss = outputs["loss"]
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(f"Loss = {loss.item()}")
+print(f"Pretrained backprop test passed!")
+print("#" * 50 + "\n")
+
+# test fine-tuning backprop
+print("Testing fine-tuning backprop...")
+for _ in range(10):
+    outputs = model(inputs, mask_ratio=0.5, stage="finetune")
+    loss = criterion(outputs["pred_cls"], targets)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(f"Loss = {loss.item()}")
+print(f"Fine-tuning backprop test passed!")
+print("#" * 50 + "\n")
+
+# test combine backprop
+print("Testing combine backprop...")
+for _ in range(10):
+    outputs = model(inputs, mask_ratio=0.5, stage="combine")
+    cls_loss = criterion(outputs["pred_cls"], targets)
+    loss = outputs["loss"] + cls_loss
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(f"Cls loss = {cls_loss.item()}, Mask loss = {outputs['loss'].item()}")
+print(f"Combine backprop test passed!")
+print("#" * 50 + "\n")
